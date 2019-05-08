0c535cc98d6f319a924de2864c0b49f3ded54b22
imrandev <context.imran@gmail.com>
2019-05-08 13:55:27 +0600

    fixed api issue

--- app/src/main/java/com/samsolution/vpngaterestdemo/Api.java
+++ app/src/main/java/com/samsolution/vpngaterestdemo/Api.java
@@ -8,7 +8,7 @@ public interface Api {
 
     //http://manage.v4v.info/api/?getserverlist&token=QklseUVlTHRYTTdEUThYTTdEUThiZVA1Unh5RWVMdHdBQUFBRXdBQUFBRUdiZVA1UnhHQkls
     String BASE_URL = "http://manage.v4v.info/";
-    
+
     @GET("api/")
     Call<ServerResponse> getServerResult(@Query(value = "token", encoded=true) String token);
 
